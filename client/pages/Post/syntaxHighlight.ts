60d53a7e0d5e945cf6b361fea79ef16bbc2aa53e
takurinton <takurinton@takurinton.com>
2022-01-16 15:12:31 +0900

    wip: og

--- client/pages/Post/syntaxHighlight.ts
+++ client/pages/Post/syntaxHighlight.ts
@@ -31,5 +31,38 @@ export const markdownStyle = () => {
                 <p class="p">${text}</p>
                 `;
     };
+    r.link = (href: string, title: string, text: string) => {
+        // if (window !== undefined) {
+        //     const html = (async () => {
+        //         try {
+        //             const res = await fetch(href, {
+        //                 headers: {
+        //                     'Content-Type': 'text/html',
+        //                 },
+        //                 mode: 'no-cors',
+        //             });
+        //             const text = await res.text();
+        //             return new DOMParser().parseFromString(text, "text/html");
+        //         } catch {
+        //             return new DOMParser().parseFromString('', "text/html");
+        //         }
+        //     })();
+
+        //     const getMetaTags = (html, m) => {
+        //         const metas = html.getElementsByTagName('meta');
+        //         for (let i = 0; i < metas.length; i++) {
+        //             if (metas[i].getAttribute('name') === m) {
+        //                 return metas[i].getAttribute('content');
+        //             }
+        //         }
+        //     }
+
+        //     html.then(h => console.log(getMetaTags(h, 'description')));
+        // }
+
+        return `
+            <a href="${href}" target="_blank">${text}</a>
+        `
+    }
     return r;
 };
