fa44340ac5a1b9c042f221ca331e18e5ddc29b02
takurinton <takurinton@takurinton.com>
2022-01-30 16:15:45 +0900

    fix: Html.tsx

--- server/render.tsx
+++ server/render.tsx
@@ -2,7 +2,7 @@ import React, { createElement } from "react";
 import ReactDOMServer from "react-dom/server";
 import { StaticRouter } from "react-router-dom/server";
 import { App } from "../client/App";
-import Html from "../client/Html";
+import Html from "./Html";
 
 export function render({
     url,
