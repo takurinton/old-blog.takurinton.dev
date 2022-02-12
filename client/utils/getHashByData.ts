df1c4ecd67dc957e65dce5449a2b3c9b07bb809f
takurinton <takurinton@takurinton.com>
2022-02-13 00:09:49 +0900

    fix

--- client/utils/getHashByData.ts
+++ client/utils/getHashByData.ts
@@ -1,10 +1,9 @@
 // 厳密には hash からとってるわけではない
 export const getHashByData = (obj, isServer) => {
-    let data;
     if (!isServer) {
         // @ts-ignore
-        data = Object.values(JSON.parse(obj))[0].data
+        return JSON.parse(Object.values(JSON.parse(obj))[0].data)
     }
     // @ts-ignore
-    return isServer ? JSON.parse(Object.values(obj)[0].data) : JSON.parse(data);
+    return JSON.parse(Object.values(obj)[0].data);
 }
