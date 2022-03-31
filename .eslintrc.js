199a32c94c7b1c5207a9e6f8ffb238ead48b57fa
takurinton <takurinton@takurinton.dev>
2022-03-31 10:34:41 +0900

    add: eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint", "@takurinton/limit-import-scope"],
+  rules: {
+    "@takurinton/limit-import-scope/": [
+      "error",
+      {
+        patterns: ["./internal/**", "utils/", "./**", "./styled"],
+      },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+  },
+};
